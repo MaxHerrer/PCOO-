88a2ae73ec5dc14001791d6ae76c4cd5faa5279b
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2023-11-27 14:05:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,198 @@
 #include <iostream>
+#include <string>
 #include "jugador.h"
 #include "equipo.h"
 #include "entrenador.h"
 
-using namespace std;
+jugador::jugador() {}
 
-string main() {
+void jugador::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
 
+void jugador::setApellidos(std::string apellidos) {
+    this->apellidos = apellidos;
+}
+
+void jugador::setNacionalidad(std::string nacionalidad) {
+    this->nacionalidad = nacionalidad;
+}
+
+void jugador::setPosicion(std::string posicion) {
+    this->posicion = posicion;
+}
+
+std::string jugador::getNombre() {
+    return nombre;
+}
+
+std::string jugador::getApellidos() {
+    return apellidos;
+}
+
+std::string jugador::getNacionalidad() {
+    return nacionalidad;
+}
+
+std::string jugador::getPosicion() {
+    return posicion;
+}
+
+void jugador::imprimeDatos() {
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "Apellidos: " << apellidos << std::endl;
+    std::cout << "Nacionalidad: " << nacionalidad << std::endl;
+    std::cout << "Posición: " << posicion << std::endl;
+}
+
+equipo::equipo() {}
+
+void equipo::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+void equipo::setPais(std::string pais) {
+    this->pais = pais;
+}
+
+void equipo::setCiudad(std::string ciudad) {
+    this->ciudad = ciudad;
+}
+
+void equipo::setDivision(std::string division) {
+    this->division = division;
+}
+
+std::string equipo::getNombre() {
+    return nombre;
+}
+
+std::string equipo::getPais() {
+    return pais;
+}
+
+std::string equipo::getCiudad() {
+    return ciudad;
+}
+
+std::string equipo::getDivision() {
+    return division;
+}
+
+void equipo::imprimeDatos() {
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "País: " << pais << std::endl;
+    std::cout << "Ciudad: " << ciudad << std::endl;
+    std::cout << "División: " << division << std::endl;
+}
+
+entrenador::entrenador(){}
+
+void entrenador::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+void entrenador::setApellidos(std::string apellidos) {
+    this->apellidos = apellidos;
+}
+
+void entrenador::setNacionalidad(std::string nacionalidad) {
+    this->nacionalidad = nacionalidad;
+}
+
+std::string entrenador::getNombre() {
+    return nombre;
+}
+
+std::string entrenador::getApellidos() {
+    return apellidos;
+}
+
+std::string entrenador::getNacionalidad() {
+    return nacionalidad;
+}
+
+void entrenador::imprimeDatos() {
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "Apellidos: " << apellidos << std::endl;
+    std::cout << "Nacionalidad: " << nacionalidad << std::endl;
+}
+
+int main() {
+    // Creación del jugador y obtención de datos del usuario
     jugador miJugador;
+    std::string nombreJugador, apellidosJugador, nacionalidadJugador, posicionJugador;
+
+    std::cout << "Datos del jugador:" << std::endl;
+    std::cout << "Escribe el nombre del jugador: ";
+    std::getline(std::cin, nombreJugador);
+    std::cout << "Escribe los apellidos del jugador: ";
+    std::getline(std::cin, apellidosJugador);
+    std::cout << "Escribe la nacionalidad del jugador: ";
+    std::getline(std::cin, nacionalidadJugador);
+    std::cout << "Escribe la posición del jugador: ";
+    std::getline(std::cin, posicionJugador);
+
+    // Establecer los datos del jugador con los valores ingresados por el usuario
+    miJugador.setNombre(nombreJugador);
+    miJugador.setApellidos(apellidosJugador);
+    miJugador.setNacionalidad(nacionalidadJugador);
+    miJugador.setPosicion(posicionJugador);
+
+    // Imprimir los datos del jugador
+    std::cout << "\nDatos del jugador:" << std::endl;
     miJugador.imprimeDatos();
 
-    string nombre;
-    string apellidos;
-    string nacionalidad;
-    string posicion;
-
-    cout << "Escribe el nombre del jugador: ";
-    cin >> nombre;
-    cout << "Escribe los apellidos del jugador: ";
-    cin >> apellidos;
-    cout << "Escribe la nacionalidad del jugador: ";
-    cin >> nacionalidad;
-    cout << "Escribe la posicion del jugador: ";
-    cin >> posicion;
-    
+    // Creación del equipo y obtención de datos del usuario
     equipo miEquipo;
+    std::string nombreEquipo, paisEquipo, ciudadEquipo, divisionEquipo;
+
+    std::cout << "\nDatos del equipo:" << std::endl;
+    std::cout << "Escribe el nombre del equipo: ";
+    std::getline(std::cin, nombreEquipo);
+    std::cout << "Escribe el país del equipo: ";
+    std::getline(std::cin, paisEquipo);
+    std::cout << "Escribe la ciudad del equipo: ";
+    std::getline(std::cin, ciudadEquipo);
+    std::cout << "Escribe la división del equipo: ";
+    std::getline(std::cin, divisionEquipo);
+
+    // Establecer los datos del equipo con los valores ingresados por el usuario
+    miEquipo.setNombre(nombreEquipo);
+    miEquipo.setPais(paisEquipo);
+    miEquipo.setCiudad(ciudadEquipo);
+    miEquipo.setDivision(divisionEquipo);
+
+    // Imprimir los datos del equipo
+    std::cout << "\nDatos del equipo:" << std::endl;
     miEquipo.imprimeDatos();
 
-    string nombre;
-    string pais;
-    string ciudad;
-    string division;
-
-    cout << "Escribe el nombre del equipo: ";
-    cin >> nombre;
-    cout << "Escribe el pais del equipo: ";
-    cin >> pais;
-    cout << "Escribe la ciudad del equipo: ";
-    cin >> ciudad;
-    cout << "Escribe la division del equipo: ";
-    cin >> division;
-
+    // Creación del entrenador y obtención de datos del usuario
     entrenador miEntrenador;
+    std::string nombreEntrenador, apellidosEntrenador, nacionalidadEntrenador;
+
+    std::cout << "\nDatos del entrenador:" << std::endl;
+    std::cout << "Escribe el nombre del entrenador: ";
+    std::getline(std::cin, nombreEntrenador);
+    std::cout << "Escribe los apellidos del entrenador: ";
+    std::getline(std::cin, apellidosEntrenador);
+    std::cout << "Escribe la nacionalidad del entrenador: ";
+    std::getline(std::cin, nacionalidadEntrenador);
+
+    // Establecer los datos del entrenador con los valores ingresados por el usuario
+    miEntrenador.setNombre(nombreEntrenador);
+    miEntrenador.setApellidos(apellidosEntrenador);
+    miEntrenador.setNacionalidad(nacionalidadEntrenador);
+
+    // Imprimir los datos del entrenador
+    std::cout << "\nDatos del entrenador:" << std::endl;
     miEntrenador.imprimeDatos();
 
-    string nombre;
-    string apellidos;
-    string nacionalidad;
+    // Transferir al jugador al equipo (ejemplo de relación)
+    std::cout << "\nTransferencia de " << miJugador.getNombre() << " al equipo " << miEquipo.getNombre() << "." << std::endl;
 
-    cout << "Escribe el nombre del entrenador: ";
-    cin >> nombre;
-    cout << "Escribe los apellidos del entrenador: ";
-    cin >> apellidos;
-    cout << "Escribe la nacionalidad del entrenador: ";
-    cin >> nacionalidad;
+    return 0;
+}
+
 }
 
