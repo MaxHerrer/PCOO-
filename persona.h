ccc532496b7f53be8a4e1bcd21b140ad35bb7f93
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2023-12-01 11:28:55 -0600

    Update persona.h

--- persona.h
+++ persona.h
@@ -4,13 +4,17 @@
 #include <iostream>
 #include <string>
 
-class persona {
-protected:
+class Persona {
+protected: 
     std::string nombre;
     std::string apellidos;
     std::string nacionalidad;
 
 public:
+    // Constructor
+    Persona();
+
+    // Métodos específicos para jugador
     void setNombre(std::string nombre);
     void setApellidos(std::string apellidos);
     void setNacionalidad(std::string nacionalidad);
@@ -19,5 +23,30 @@ public:
     std::string getNacionalidad();
     void imprimeDatos();
 };
+
+// Implementación de los métodos de persona
+void Persona::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+void Persona::setApellidos(std::string apellidos) {
+    this->apellidos = apellidos;
+}
+
+void Persona::setNacionalidad(std::string nacionalidad) {
+    this->nacionalidad = nacionalidad;
+}
+
+std::string Persona::getNombre() {
+    return nombre;
+}
+
+std::string Persona::getApellidos() {
+    return apellidos;
+}  
+
+std::string Persona::getNacionalidad() {
+    return nacionalidad;
+}
 
 #endif /* PERSONA_H */
