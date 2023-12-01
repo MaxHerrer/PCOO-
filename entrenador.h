2c101f0135ce877d6706011cc9a7ffa2bc3eeb35
MaxHerrer <142357090+MaxHerrer@users.noreply.github.com>
2023-12-01 11:26:34 -0600

    Update entrenador.h

--- entrenador.h
+++ entrenador.h
@@ -1,16 +1,39 @@
 #ifndef ENTRENADOR_H
 #define ENTRENADOR_H
 
-#include "persona.h"
+#include "Persona.h"
 #include <iostream>
 #include <string>
 
-class entrenador : public persona {
+class Entrenador : public Persona {
+private:
+    std::string ganados;
 public:
     // Constructor
-    entrenador();
-    // Método para establecer y obtener datos del entrenador
+    Entrenador();
+
+    // Método para establecer y obtener datos del entrenador agregar un atributo
+    void setGanados(std::string ganados);
+    std::string getGanados();
     void imprimeDatos();
 };
+
+// Implementación de los métodos de entrenador
+
+Entrenador::Entrenador() {}
+
+void Entrenador::setGanados(std::string ganados) {
+    this->ganados = ganados;
+}
+
+std::string Entrenador::getGanados() {
+    return ganados;
+}
+
+void Entrenador::imprimeDatos() {
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "Apellidos: " << apellidos << std::endl;
+    std::cout << "Nacionalidad: " << nacionalidad << std::endl;
+}
 
 #endif /* ENTRENADOR_H */
